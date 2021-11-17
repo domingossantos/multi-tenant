212282af140e0488f979164637da45f995628d04
Domingos Santos <domsantos@gmail.com>
2021-11-17 10:26:03 -0300

    Inicial

---
+++ src/test/java/br/com/domsantos/multitenant/MultiTenantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.domsantos.multitenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
